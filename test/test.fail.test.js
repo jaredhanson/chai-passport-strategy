801fc909a8178535399090534b25f00bc045f9be
Jared Hanson <jaredhanson@gmail.com>
2013-08-01 20:53:58 -0700

    Test case for fail callback.

---
+++ test/test.fail.test.js
@@ -0,0 +1,40 @@
+var Test = require('../lib/Test');
+
+describe('test strategy that calls fail', function() {
+  
+  function Strategy() {}
+  Strategy.prototype.authenticate = function() {
+    this.fail('realm="foo"', 401);
+  }
+  
+  describe('with a fail callback', function() {
+    var challenge
+      , status;
+    
+    before(function(done) {
+      var test = new Test(new Strategy());
+      test.fail(function(c, s) {
+        challenge = c;
+        status = s;
+        done();
+      }).authenticate();
+    });
+    
+    it('should call fail callback', function() {
+      expect(challenge).to.be.equal('realm="foo"')
+      expect(status).to.equal(401);
+    });
+  });
+  
+  describe('without a fail callback', function() {
+    
+    it('should throw an error', function() {
+      expect(function() {
+        var test = new Test(new Strategy());
+        test.authenticate();
+      }).to.throw(Error, 'Strategy#fail should not be called');
+    });
+    
+  });
+  
+});

95c6c501a4fbc02f3335c2b4df13e7feaa06d646
Jared Hanson <jaredhanson@gmail.com>
2013-08-01 20:39:46 -0700

    Document Test and Request.

---
+++ test/request.test.js
@@ -0,0 +1,14 @@
+var Request = require('../lib/request');
+
+describe('Request', function() {
+  
+  var req = new Request();
+  
+  it('should be constructed with default properties', function() {
+    expect(Object.keys(req)).to.have.length(3);
+    expect(req.method).to.equal('GET');
+    expect(req.url).to.equal('/');
+    expect(req.headers).to.be.an.object;
+  });
+  
+});

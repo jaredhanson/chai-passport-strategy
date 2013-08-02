4af0992bf296a301d767df9f4765232d815313c5
Jared Hanson <jaredhanson@gmail.com>
2013-08-01 20:56:39 -0700

    Test case for success callback.

---
+++ test/test.success.test.js
@@ -0,0 +1,40 @@
+var Test = require('../lib/Test');
+
+describe('test strategy that calls success', function() {
+  
+  function Strategy() {}
+  Strategy.prototype.authenticate = function() {
+    this.success({ id: '1234'}, { scope: 'read' });
+  }
+  
+  describe('with a success callback', function() {
+    var user
+      , info;
+    
+    before(function(done) {
+      var test = new Test(new Strategy());
+      test.success(function(u, i) {
+        user = u;
+        info = i;
+        done();
+      }).authenticate();
+    });
+    
+    it('should call success callback', function() {
+      expect(user.id).to.be.equal('1234')
+      expect(info.scope).to.be.equal('read');
+    });
+  });
+  
+  describe('without a success callback', function() {
+    
+    it('should throw an error', function() {
+      expect(function() {
+        var test = new Test(new Strategy());
+        test.authenticate();
+      }).to.throw(Error, 'Strategy#success should not be called');
+    });
+    
+  });
+  
+});

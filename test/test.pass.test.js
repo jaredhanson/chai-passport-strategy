af9a65a0b36d50e1661e4104ece320cd46d1f9f1
Jared Hanson <jaredhanson@gmail.com>
2013-08-01 20:51:28 -0700

    Test case for redirect, pass, and error callbacks.

---
+++ test/test.pass.test.js
@@ -0,0 +1,37 @@
+var Test = require('../lib/Test');
+
+describe('test strategy that calls pass', function() {
+  
+  function Strategy() {}
+  Strategy.prototype.authenticate = function() {
+    this.pass();
+  }
+  
+  describe('with a pass callback', function() {
+    var ok;
+    
+    before(function(done) {
+      var test = new Test(new Strategy());
+      test.pass(function() {
+        ok = true;
+        done();
+      }).authenticate();
+    });
+    
+    it('should call pass callback', function() {
+      expect(ok).to.be.true;
+    });
+  });
+  
+  describe('without a pass callback', function() {
+    
+    it('should throw an error', function() {
+      expect(function() {
+        var test = new Test(new Strategy());
+        test.authenticate();
+      }).to.throw(Error, 'Strategy#pass should not be called');
+    });
+    
+  });
+  
+});
---
+++ test/test.redirect.test.js
@@ -0,0 +1,40 @@
+var Test = require('../lib/Test');
+
+describe('test strategy that calls redirect', function() {
+  
+  function Strategy() {}
+  Strategy.prototype.authenticate = function() {
+    this.redirect('/login', 401);
+  }
+  
+  describe('with a redirect callback', function() {
+    var url
+      , status;
+    
+    before(function(done) {
+      var test = new Test(new Strategy());
+      test.redirect(function(u, s) {
+        url = u;
+        status = s;
+        done();
+      }).authenticate();
+    });
+    
+    it('should call redirect callback', function() {
+      expect(url).to.be.equal('/login')
+      expect(status).to.equal(401);
+    });
+  });
+  
+  describe('without a redirect callback', function() {
+    
+    it('should throw an error', function() {
+      expect(function() {
+        var test = new Test(new Strategy());
+        test.authenticate();
+      }).to.throw(Error, 'Strategy#redirect should not be called');
+    });
+    
+  });
+  
+});

6bbe60bbbc605ba4a7deeee616cf98355872ece1
Jared Hanson <jaredhanson@gmail.com>
2016-01-29 11:57:52 -0800

    Simplify tests.

--- test/package.test.js
+++ test/package.test.js
@@ -1,11 +1,30 @@
 /* global describe, it, expect */
 
-var passport = require('..');
+var helper = require('..')
+  , Test = require('../lib/test');
 
 describe('chai-passport-strategy', function() {
   
   it('should export function', function() {
-    expect(passport).to.be.a('function');
+    expect(helper).to.be.a('function');
+  });
+  
+  describe('used as a chai helper', function() {
+    var chai = {};
+    helper(chai);
+  
+    it('should add passport helper to chai', function() {
+      expect(chai.passport).to.be.a('object');
+      expect(chai.passport.use).to.be.a('function');
+    });
+  
+    describe('when invoked', function() {
+      var test = chai.passport.use({});
+    
+      it('should return test wrapper', function() {
+        expect(test).to.be.an.instanceOf(Test);
+      });
+    });
   });
   
 });
